e1c693d713306d6ed5b71b47265240e3982ab847
Alexander Lopatin <alopatindev@gmail.com>
2018-07-04 01:14:52 +0300

    bugfixes

--- sync-audio-tracks.sh
+++ sync-audio-tracks.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/sh
 
 set -e
 
@@ -19,16 +19,36 @@ downsample_and_cut ()
     ffmpeg -y -hide_banner -loglevel error -i "$1" -ab 8 -ar 8000 -t 900 -map 0:a -ac 1 -vn "$2"
 }
 
-TMPDIR=$(mktemp -d /var/tmp/tmp.XXXXXXXXXX)
-COMPUTE_OFFSET="$(dirname $0)/compute_sound_offset"
+insert_silence ()
+{
+    sox --no-show-progress --no-dither "$1" "$2" pad "$3" 0
+}
 
-TMP_GOOD_SOUND="${TMPDIR}/${GOOD_SOUND}.wav"
-TMP_BAD_SOUND="${TMPDIR}/${BAD_SOUND}.wav"
+remove_beginning ()
+{
+    sox --no-show-progress --no-dither "$1" "$2" trim "$3"
+}
+
+TMPDIR=$(mktemp -d /var/tmp/tmp.XXXXXXXXXX)
+COMPUTE_OFFSET="$(dirname $(realpath $0))/compute_sound_offset"
+
+TMP_GOOD_SOUND="${TMPDIR}/$(basename ${GOOD_SOUND}).wav"
+TMP_BAD_SOUND="${TMPDIR}/$(basename ${BAD_SOUND}).wav"
 downsample_and_cut "${GOOD_SOUND}" "${TMP_GOOD_SOUND}"
 downsample_and_cut "${BAD_SOUND}" "${TMP_BAD_SOUND}"
 
 OFFSET=$(${COMPUTE_OFFSET} "${TMP_GOOD_SOUND}" "${TMP_BAD_SOUND}")
 echo "offset is ${OFFSET} seconds"
-sox --no-show-progress --no-dither "${GOOD_SOUND}" "${OUTPUT}" pad "${OFFSET}" 0
+insert_silence "${GOOD_SOUND}" "${OUTPUT}" "${OFFSET}"
 
-rm -rfv "${TMPDIR}"
+# FIXME: compute_sound_offset doesn't determine offset sign
+TMP_OUTPUT="${TMPDIR}/$(basename ${OUTPUT}).wav"
+downsample_and_cut "${OUTPUT}" "${TMP_OUTPUT}"
+NEW_OFFSET=$(${COMPUTE_OFFSET} "${TMP_OUTPUT}" "${TMP_BAD_SOUND}")
+if (( $(echo "${NEW_OFFSET} > ${OFFSET}" | bc -l) )) ; then
+    remove_beginning "${GOOD_SOUND}" "${OUTPUT}" "${OFFSET}"
+fi
+
+rm -rf "${TMPDIR}"
+
+echo ok
